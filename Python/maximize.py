3c4ff2b483f9da2011d9db52b37f451a7d142e58
nik-6174 <78644716+nik-6174@users.noreply.github.com>
2022-10-19 10:26:02 +0530

    The code maximizes the number S
    The following question was solved by me in an hackerrank Question, S is the reminder when the sum of the squares of elements from each of K rows is divided by a number M.
    The code maximizes the value of S and prints it.

---
+++ Python/maximize.py
@@ -0,0 +1,15 @@
+# f(X) = X^2
+# S = (f(X1) + f(X2) + ... + f(XK))%M
+# The Code maximizes S taking the values of f(X) from each of K lines
+# Example input:
+# 3 1000
+# 2 5 4
+# 3 7 8 9
+# 5 5 7 8 9 10
+# output : 206
+
+
+from itertools import product
+K, M = map(int, input("Input K & M : ").split())
+List = [[int(i)**2 for i in input().split()[1:]] for _ in range(K)]
+print("The maximum value is :", max([sum(r) % M for r in product(*List)]))
